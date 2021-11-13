d6ff9120203240767a05e0338f592beecf26fa30
Aleksandr Suchugov <alex.coup@yandex.ru>
2021-11-13 19:29:30 +0300

    code enhancements added

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,13 +3,17 @@ const mainElmsCount = mainMenuElms.length;
 
 for (let i = 0; i < mainElmsCount; i++){
     mainMenuElms.item(i).addEventListener('click', function(event) { 
-        event.preventDefault();
         let activeMenu = document.querySelector('.menu_active');
+        let subMenuItem = this.querySelector('ul.menu_sub');
+        if (subMenuItem === activeMenu) {
+            activeMenu.classList.remove('menu_active');
+            return;
+        }
         if (activeMenu) {
             activeMenu.classList.remove('menu_active');
         }
-        let subMenuItem = this.querySelector('ul.menu_sub');
         if (subMenuItem) {
+           event.preventDefault();
            return subMenuItem.classList.add('menu_active');
         }
     });
