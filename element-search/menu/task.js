ea6e1d4f2c8a565ad5a4250d1b574d929ebeb88d
Aleksandr Suchugov <alex.coup@yandex.ru>
2021-11-14 10:55:38 +0300

    fix for menu task

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,20 +1,23 @@
 const mainMenuElms = document.querySelectorAll("ul.menu_main > li");
 const mainElmsCount = mainMenuElms.length;
 
-for (let i = 0; i < mainElmsCount; i++){
+for (let i = 0; i < mainElmsCount; i++) {
     mainMenuElms.item(i).addEventListener('click', function(event) { 
         let activeMenu = document.querySelector('.menu_active');
-        let subMenuItem = this.querySelector('ul.menu_sub');
-        if (subMenuItem === activeMenu) {
+        let subMenuItem = this.querySelector('ul.menu_sub');              
+        if (subMenuItem === activeMenu && activeMenu) {
+            event.preventDefault();
             activeMenu.classList.remove('menu_active');
             return;
-        }
-        if (activeMenu) {
-            activeMenu.classList.remove('menu_active');
-        }
+        }       
         if (subMenuItem) {
-           event.preventDefault();
-           return subMenuItem.classList.add('menu_active');
+            event.preventDefault();
+            if (activeMenu) {
+                activeMenu.classList.remove('menu_active');          
+            }
+            subMenuItem.classList.add('menu_active');
+            return;
         }
+        return;
     });
 }
